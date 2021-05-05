77f5ce79119fa13d91eb53f53aac0489582f863a
sorshireality <tutunikssa@gmail.com>
2021-05-05 18:06:09 +0300

    Initial Commit

---
+++ nyan_warrior/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  nyan_warrior
+//
+//  Created by Александр Тютюнник on 05.05.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ nyan_warrior/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  nyan_warrior
+//
+//  Created by Александр Тютюнник on 05.05.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
